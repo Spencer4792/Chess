50c1991674952d0f5d3cdb48c2b13532419ea2f4
Spencer Hales <112470587+Spencer4792@users.noreply.github.com>
2026-01-08 20:32:10 -0700

    Added websocket handling and fixed the annoying issue where the server wouldn't recognize the handler.

---
+++ client/src/main/java/client/Main.java
@@ -0,0 +1,11 @@
+package client;
+
+public class Main {
+    public static void main(String[] args) {
+        String serverUrl = "http://localhost:8080";
+        if (args.length == 1) {
+            serverUrl = args[0];
+        }
+        new ChessClient(serverUrl).run();
+    }
+}
